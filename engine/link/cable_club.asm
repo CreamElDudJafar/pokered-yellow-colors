f722e7a22b38b54bfd6c24f43d98deee628a8bdc
Sylvie <35663410+Rangi42@users.noreply.github.com>
2024-09-18 21:41:58 -0400

    Identify remaining uses of `wd0b5` and `wd11e` (#463)

--- engine/link/cable_club.asm
+++ engine/link/cable_club.asm
@@ -668,7 +668,7 @@ TradeCenter_PrintPartyListNames:
 	ld a, [de]
 	cp $ff
 	ret z
-	ld [wd11e], a
+	ld [wNamedObjectIndex], a
 	push bc
 	push hl
 	push de
@@ -705,7 +705,7 @@ TradeCenter_Trade:
 	ld b, 0
 	add hl, bc
 	ld a, [hl]
-	ld [wd11e], a
+	ld [wNamedObjectIndex], a
 	call GetMonName
 	ld hl, wNameBuffer
 	ld de, wNameOfPlayerMonToBeTraded
@@ -717,7 +717,7 @@ TradeCenter_Trade:
 	ld b, 0
 	add hl, bc
 	ld a, [hl]
-	ld [wd11e], a
+	ld [wNamedObjectIndex], a
 	call GetMonName
 	ld hl, WillBeTradedText
 	bccoord 1, 14
