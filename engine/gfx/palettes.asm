45ffc58d6cb4721ab4f98909fe64e40d0788f42f
CreamElDudJafar <jahern2398@gmail.com>
2025-09-06 21:14:19 -0500

    Removed Unused Surfing Pikachu Palettes
    Unused in Red and Blue

--- engine/gfx/palettes.asm
+++ engine/gfx/palettes.asm
@@ -240,16 +240,6 @@ SetPal_TrainerCard:
 	ld de, wTrainerCardBlkPacket
 	ret
 
-SetPal_PikachusBeach::
-	ld hl, PalPacket_PikachusBeach
-	ld de, BlkPacket_WholeScreen
-	ret
-
-SetPal_PikachusBeachTitle::
-	ld hl, PalPacket_PikachusBeachTitle
-	ld de, UnknownPacket_72751
-	ret
-
 SetPalFunctions:
 ; entries correspond to SET_PAL_* constants
 	dw SetPal_BattleBlack
@@ -266,8 +256,6 @@ SetPalFunctions:
 	dw SetPal_PokemonWholeScreen
 	dw SetPal_GameFreakIntro
 	dw SetPal_TrainerCard
-	dw SetPal_PikachusBeach
-	dw SetPal_PikachusBeachTitle
 
 ; The length of the blk data of each badge on the Trainer Card.
 ; The Rainbow Badge has 3 entries because of its many colors.
