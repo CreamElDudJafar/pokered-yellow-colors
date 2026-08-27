4f01b05bb8fe0316ac158cc92d134110edd96ae9
CreamElDudJafar <jahern2398@gmail.com>
2026-08-27 13:26:22 -0500

    Fix CGB/GBC Color Stuff

--- home/copy2.asm
+++ home/copy2.asm
@@ -35,6 +35,46 @@ FarCopyData3::
 	ld [rROMB], a
 	ret
 
+FarCopyData4::
+	ldh [hROMBankTemp], a
+	ldh a, [hLoadedROMBank]
+	push af
+	ldh a, [hROMBankTemp]
+	ldh [hLoadedROMBank], a
+	ld [rROMB], a
+	call CheckSpecialCopyData
+	pop af
+	ldh [hLoadedROMBank], a
+	ld [rROMB], a
+	ret
+
+CheckSpecialCopyData:
+	push af
+	ldh a, [hFlagsFFFA]
+	bit 3, a
+	jr nz, .specialCopy
+	pop af
+	call CopyData
+	ret
+.specialCopy
+	pop af
+
+SpecialCopyData:
+	di
+.waitVRAM
+	ldh a, [rSTAT]
+	and %10
+	jr nz, .waitVRAM
+	ld a, [hli]
+	ld [de], a
+	inc de
+	dec bc
+	ei
+	ld a, c
+	or b
+	jr nz, SpecialCopyData
+	ret
+
 FarCopyDataDouble::
 ; Expand bc bytes of 1bpp image data
 ; from a:hl to 2bpp data at de.
--- home/overworld.asm
+++ home/overworld.asm
@@ -748,6 +748,7 @@ ExtraWarpCheck::
 	jp Bankswitch
 
 MapEntryAfterBattle::
+	call DelayFrame
 	farcall IsPlayerStandingOnWarp ; for enabling warp testing after collisions
 	ld a, [wMapPalOffset]
 	and a
@@ -878,7 +879,7 @@ LoadTilesetTilePatternData::
 	ld de, vTileset
 	ld bc, MAP_TILESET_SIZE tiles
 	ld a, [wTilesetBank]
-	jp FarCopyData2
+	jp FarCopyData4
 
 ; this loads the current map's complete tile map (which references blocks, not individual tiles) to wOverworldMap
 ; it can also load partial tile maps of connected maps into a border of length 3 around the current map
@@ -2294,7 +2295,6 @@ CopyMapConnectionHeader::
 LoadMapData::
 	ldh a, [hLoadedROMBank]
 	push af
-	call DisableLCD
 	ld a, HIGH(vBGMap0)
 	ld [wMapViewVRAMPointer + 1], a
 	xor a
@@ -2307,6 +2307,8 @@ LoadMapData::
 	ld [wSpriteSetID], a
 	call LoadTextBoxTilePatterns
 	call LoadMapHeader
+	ld hl, hFlagsFFFA
+	set 3, [hl]
 	farcall InitMapSprites ; load tile pattern data for sprites
 	call LoadTileBlockMap
 	call LoadTilesetTilePatternData
@@ -2316,13 +2318,11 @@ LoadMapData::
 	ld de, vBGMap0
 	ld b, SCREEN_HEIGHT
 .vramCopyLoop
+	push bc
+	ld b, 0
 	ld c, SCREEN_WIDTH
-.vramCopyInnerLoop
-	ld a, [hli]
-	ld [de], a
-	inc e
-	dec c
-	jr nz, .vramCopyInnerLoop
+	call SpecialCopyData
+	pop bc
 	ld a, TILEMAP_WIDTH - SCREEN_WIDTH
 	add e
 	ld e, a
@@ -2333,7 +2333,8 @@ LoadMapData::
 	jr nz, .vramCopyLoop
 	ld a, $01
 	ld [wUpdateSpritesEnabled], a
-	call EnableLCD
+	ld hl, hFlagsFFFA
+	res 3, [hl]
 	ld b, SET_PAL_OVERWORLD
 	call RunPaletteCommand
 	call LoadPlayerSpriteGraphics
